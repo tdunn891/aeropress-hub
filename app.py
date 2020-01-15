6af012803fe7cf43109d0b6affcc44af577c2bea
tdunn891 <thomas.dunn@uqconnect.edu.au>
2020-01-15 00:27:16 +0000

    Add insert_brew app route to app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -26,13 +27,52 @@ def get_brews():
                            brews=mongo.db.brews.find())
 
 
-@app.route('/insert_brew', methods=['POST', 'GET'])
+@app.route('/add_brew')
+def add_brew():
+    return render_template("add_brew.html")
+
+
+@app.route('/insert_brew', methods=['GET', 'POST'])
 def insert_brew():
-    # brews = mongo.db.brews
-    # brews.insert_one(request.form.to_dict())
-    return render_template("insert_brew.html")
-    # return redirect(url_for('get_brews'))
-    # TODO: correct
+    brews = mongo.db.brews
+    if request.method == 'POST':
+        req = request.form
+        # Get in right structure
+        # Prepare payload
+        payload = {
+            "brew_id": 100,
+            "brew_name": req.get("brew_name"),
+            "barista": req.get("barista_name"),
+            "brew_source": "World AeroPress Champion Finalist",
+            # TODO: deal with steps
+            "steps": [
+                "Test step 1",
+                "Test step 2"
+            ],
+            # TODO: convert secs to minutes
+            "total_brew_time": req.get("brew_time") + "s",
+            "details": {
+                "coffee": req.get("coffee_weight") + "g",
+                "grind": req.get("grind_size") + "/10",
+                "water": req.get("water_temp") + " C",
+                "brewer": req.get("brewer"),
+                "filter": req.get("filter")
+            },
+            "notes": req.get("notes"),
+            "likes": "",
+            # TODO: deal with img_url, or remove
+            "img_url": ""
+        }
+        # print(request.json)
+    brews.insert_one(payload)
+    return redirect(url_for('get_brews'))
+
+# TODO: test delete
+@app.route('/delete_brew/<brew_id>')
+def delete_brew():
+    brews = mongo.db.brews
+    brews.remove()({'id_': ObjectId(brew_id)})
+    return redirect(url_for('get_brews'))
 
 
 if __name__ == '__main__':
